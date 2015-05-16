9a28b2a96f6b6e0cc641b2f16b8030e538cf73c1
Eko <elias.kotlyar@gmail.com>
2015-05-16 22:09:49 +0200

    Corrected Amazon Rest Service

--- src/AmazonRestService.php
+++ src/AmazonRestService.php
@@ -1,19 +1,45 @@
 <?php
 namespace Twinsen\AmazonMwsRepricing;
+
 use Sunra\PhpSimple\HtmlDomParser;
-class AmazonRestService{
-    public function getSellerName($sellerId,$marketPlace="A1PA6795UKMFR9"){
-        $url = "http://www.amazon.de/gp/aag/main?ie=UTF8&marketplaceID=".$marketPlace."&orderID=&seller=".$sellerId;
+
+class AmazonRestService
+{
+    protected $lastUrl;
+
+    public function getSellerName($sellerId, $marketPlace = "A1PA6795UKMFR9")
+    {
+        $url = "http://www.amazon.de/gp/aag/main?ie=UTF8&marketplaceID=" . $marketPlace . "&orderID=&seller=" . $sellerId;
         //echo $url;
-        $html =  file_get_contents($url);
-        $dom = HtmlDomParser::str_get_html( $html);
-        $UlElement = $dom->find('ul.aagLegalData', 0);
-        $LiElement = $UlElement->find('li.aagLegalRow', 0);
-        $LiElement->children(0)->innertext = "";
-        //echo
-        $sellerName = $LiElement->plaintext;
+        $this->lastUrl = $url;
+        $html = file_get_contents($url);
+        $dom = HtmlDomParser::str_get_html($html);
+        $sellerName = null;
+        $rightColumn = $dom->find('.amabot_right',0);
+
+
+        $UlElement = $rightColumn->find('ul.aagLegalData', 0);
+        if($UlElement != null){
+            $LiElement = $UlElement->find('li.aagLegalRow', 0);
+            if($LiElement != null){
+                $LiElement->children(0)->innertext = "";
+                $sellerName = $LiElement->plaintext;
+            }
+        }else{
+            $pElement = $rightColumn->find('p', 0);
+            $pElement->children(0)->innertext = "";
+            $sellerName = $pElement->plaintext;
+        }
+
+
+
         return $sellerName;
 
+    }
+
+    public function getLastUrl()
+    {
+        return $this->lastUrl;
     }
 
 }
