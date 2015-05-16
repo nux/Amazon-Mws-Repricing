9a6752fd5ff6edc99bbdf67921e2c0dc96c59e08
Eko <elias.kotlyar@gmail.com>
2015-05-16 21:55:43 +0200

    Corrected URL Bug.

--- src/AmazonRestService.php
+++ src/AmazonRestService.php
@@ -3,8 +3,8 @@ namespace Twinsen\AmazonMwsRepricing;
 use Sunra\PhpSimple\HtmlDomParser;
 class AmazonRestService{
     public function getSellerName($sellerId,$marketPlace="A1PA6795UKMFR9"){
-        $url = "http://www.amazon.de/gp/aag/main?ie=UTF8&marketplaceID=".$marketPlace."&orderID=&seller=ARORO09S8NEJB";
-        echo $url;
+        $url = "http://www.amazon.de/gp/aag/main?ie=UTF8&marketplaceID=".$marketPlace."&orderID=&seller=".$sellerId;
+        //echo $url;
         $html =  file_get_contents($url);
         $dom = HtmlDomParser::str_get_html( $html);
         $UlElement = $dom->find('ul.aagLegalData', 0);
